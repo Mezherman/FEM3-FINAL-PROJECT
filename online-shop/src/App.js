b25a6b2424a2615edde8fb1c23549746dc68379b
Anna Butrimenko <annettt@gmail.com>
2019-12-13 18:11:01 +0200

    Add header test

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from "./Header/header";
 
 export default class App extends Component {
   render() {
@@ -6,6 +7,7 @@ export default class App extends Component {
   console.log("TEST FOR DIRECT PUSH INTO DEV BRANCH");
     return (
       <React.Fragment>
+        <Header/>
       <h1>Hello, World</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur delectus doloribus ipsam molestiae necessitatibus nisi, non sint. Ad corporis dignissimos earum, hic libero nisi non numquam quaerat quisquam sapiente voluptatem.</p>
       </React.Fragment>
---
+++ online-shop/src/Header/header.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+class Header extends React.Component {
+  render() {
+    return <div>This is HEADER</div>;
+  }
+}
+
+export default Header;
