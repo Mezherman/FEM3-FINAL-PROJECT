71bbd3c20fa700a00cd0fa20da6ed7a7273a57e2
Anna Butrimenko <annettt@gmail.com>
2019-12-13 17:55:12 +0200

    Add paragraph

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -4,6 +4,11 @@ export default class App extends Component {
   render() {
   console.log("GO, IT Monsters!");
   console.log("TEST FOR DIRECT PUSH INTO DEV BRANCH");
-    return <h1>Hello, World</h1>
+    return (
+      <React.Fragment>
+      <h1>Hello, World</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur delectus doloribus ipsam molestiae necessitatibus nisi, non sint. Ad corporis dignissimos earum, hic libero nisi non numquam quaerat quisquam sapiente voluptatem.</p>
+      </React.Fragment>
+    )
   }
 }
