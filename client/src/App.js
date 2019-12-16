7aaa4ab58f50d74274d08fdb6575c4d4826504b5
mezherman <vlad.mezhik@gmail.com>
2019-12-17 00:43:06 +0200

    new CRA folder created, backend added, MongoDB added

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    console.log('GO, IT Monsters!');
+    return <h1>Hello, World</h1>
+  }
+}
