23880425b5fb4952afb71ea87ed388648a461ad6
mezherman <vlad.mezhik@gmail.com>
2019-12-19 20:53:19 +0200

    eslint update, meeting minutes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  state = {};
+
   render() {
     console.log('GO, IT Monsters!');
     return <h1>Hello, World</h1>
