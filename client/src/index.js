5840c2db97b8ca004929e949388dbc6a892be3ac
Anna Butrimenko <annettt@gmail.com>
2019-12-20 18:32:07 +0200

    Fix error in the folders name

--- client/src/index.js
+++ client/src/index.js
@@ -7,15 +7,15 @@ import { createMuiTheme } from '@material-ui/core';
 import App from './App';
 
 const theme = createMuiTheme({
-  breakpoints: {
-    values: {
-      xs: '0',
-      sm: '600',
-      md: '960',
-      lg: '1280',
-      xl: '1920',
-},
-  },
+  // breakpoints: {
+  //   values: {
+  //     xs: '0',
+  //     sm: '600',
+  //     md: '960',
+  //     lg: '1280',
+  //     xl: '1920',
+  //   },
+  // },
   palette: {
     primary: {
       // light: '#333',
