3a9d380ec6e09a167c972cbfed4c7e08e77ecbea
Peaceful Warrior <peaceful11111111@gmail.com>
2019-12-22 10:58:06 +0200

    carousel

--- client/src/components/Nuka-carousel/nukaCarousel.js
+++ client/src/components/Nuka-carousel/nukaCarousel.js
@@ -4,14 +4,17 @@ import Carousel from 'nuka-carousel';
 export default class NukaCarousel extends Component {
   render () {
     return (
-      <Carousel autoplay={true} autoplayInterval={1000} slideIndex={5} slidesToShow={4}  >
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide1"/>
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide2"/>
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide3"/>
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide4"/>
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide5"/>
-        <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide6"/>
-      </Carousel>
+      <div>
+        <Carousel autoplay={true} autoplayInterval={1000} wrapAround={true} slideIndex={1}
+          slidesToShow={3}>
+          <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide1"/>
+          <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide2"/>
+          <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide3"/>
+          <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide4"/>
+          <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide5"/>
+          <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide6"/>
+        </Carousel>
+      </div>
     );
   }
 }
