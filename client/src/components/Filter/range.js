6b3d617a1692fcac484c94b95426a953c43161a5
Maksym Pliushch <makcldsnetua@gmail.com>
2019-12-24 01:02:57 +0200

    refactoring filter + add search

--- client/src/components/Filter/range.js
+++ client/src/components/Filter/range.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
-import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles({
-  root: {
-    width: 300
-  }
-});
 export default function RangeSlider({ max }) {
-  const classes = useStyles();
   const [value, setValue] = React.useState([0, max]);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
   return (
-    <div className={classes.root}>
+    <>
       <Slider
         max={max}
         valueLabelDisplay="on"
@@ -24,19 +16,10 @@ export default function RangeSlider({ max }) {
         onChange={handleChange}
         aria-labelledby="range-slider"
       />
-      <Button
-        size="large"
-        fullWidth
-        variant="contained"
-        color="primary"
-        disableElevation
-        onClick={() => console.log('Apply from FILTER')}
-      >
-        Apply
-      </Button>
-    </div>
+    </>
   );
 }
+
 RangeSlider.propTypes = {
-  max: PropTypes.number.isRequired
+  max: PropTypes.number
 }
---
+++ client/src/components/Search/search.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import SearchIcon from '@material-ui/icons/Search';
+import getAllCards from '../../services/dataBase'
+
+export default class Search extends Component {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    getAllCards()
+      .then((data) => this.setState({
+        products: data.products
+      }))
+  }
+
+  render () {
+    const { products } = this.state;
+    return (
+
+      <div style={{
+        width: '500px',
+        position: 'relative'
+      }}
+      >
+        <SearchIcon style={{
+          height: '100%',
+          position: 'absolute',
+          pointerEvents: 'none',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        />
+        <Autocomplete
+          freeSolo
+          options={products.map((option) => option.title)}
+          renderInput={(params) => (
+            <TextField
+              fullWidth
+              {...params}
+              variant="outlined"
+              margin="normal"
+              placeholder="Search..."
+            />
+          )}
+        />
+
+      </div>
+    )
+  }
+}
