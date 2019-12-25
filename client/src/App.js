1533ba02835da41ca32c0bd92af77358e82bfd27
mezherman <vlad.mezhik@gmail.com>
2019-12-26 01:25:43 +0200

    tech

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ export default class App extends Component {
     return (
       <>
         <Header />
-        <HeaderNavbar />
         <Carousels
           isProductSlider={false}
           autoPlay
