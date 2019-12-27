5629c5e01bbf012520238e87a45ee71a749a8bff
mezherman <vlad.mezhik@gmail.com>
2019-12-27 16:21:58 +0200

    modal template created

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import CategoryList from './components/Category-list/category-list'
 import Footer from './components/Footer/footer'
 import ProductList from './components/Product-list/product-list';
 import ContainerFilter from './components/Filter/filter';
-import HeaderNavbar from './components/Header-navbar/header-navbar';
 import Carousels from './components/Nuka-carousel/nukaCarousel';
 import Catalog from './components/Catalog/catalog';
 import ExampleColor from './components/Example-color/example-color';
