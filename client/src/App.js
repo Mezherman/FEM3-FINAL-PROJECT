8be703fa2b7409a83c16d6a18af0f69e409f4530
Anna Butrimenko <annettt@gmail.com>
2020-01-08 22:16:27 +0200

    Add maxWidth props into Container of the App

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import ScrollTop, { ScrollToAnchor } from './components/Scroll-top/scroll-top';
 export default class App extends Component {
   render() {
     return (
-      <Container>
+      <Container maxWidth="xl">
         <Router>
           <Header />
           <ScrollToAnchor />
