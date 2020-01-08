7a6e66589e3f3f7f63e6c750a12e33ba5edb448d
unknown <makcldsnetua@gmail.com>
2020-01-08 16:41:18 +0200

    navbar uncomment

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -181,7 +181,7 @@ export default function Header() {
       {renderMenu}
       {/* {renderSearchInput} */}
       <Search />
-      {/*<HeaderNavbar />*/}
+      <HeaderNavbar />
 
     </Container>
   );
