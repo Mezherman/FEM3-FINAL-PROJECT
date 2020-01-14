fc75e99e185bf81da54af15efbefbd5b7323ead7
Peaceful Warrior <peaceful11111111@gmail.com>
2020-01-14 22:31:54 +0200

    commit before merging

--- client/src/components/Autorization-block/authorization.js
+++ client/src/components/Autorization-block/authorization.js
@@ -6,11 +6,10 @@ import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-// import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-
+import RoutesName from '../../routes-list';
 import { useStylesSingIn } from './_authorization';
 
 export default function SignIn() {
@@ -62,7 +61,7 @@ export default function SignIn() {
           >
             Sign In
           </Button>
-          <Link variant="body2" className={classes.text}>
+          <Link variant="body2" className={classes.text} href={RoutesName.signUp}>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
             Don't have an account?
             {' '}
