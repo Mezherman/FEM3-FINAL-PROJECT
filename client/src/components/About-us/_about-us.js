e52e010667b0bdf1029832fc370ba74a01995a05
unknown <makcldsnetua@gmail.com>
2020-01-15 21:53:32 +0200

    add video about wmf

--- client/src/components/About-us/_about-us.js
+++ client/src/components/About-us/_about-us.js
@@ -3,6 +3,13 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
   img: {
     width: '100%'
+  },
+  text: {
+    textAlign: 'justify'
+  },
+  video: {
+    width: '100%',
+    height: '100%'
   }
 });
 
