1b494d5c3ef245d76c12a029d1817f64e38d449b
Anna Butrimenko <annettt@gmail.com>
2020-01-22 10:36:58 +0200

    Added collapsing item into sideNavbar

--- client/src/components/Header-navbar/Navbar/navbar.js
+++ client/src/components/Header-navbar/Navbar/navbar.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Menu, MenuItem, Button, Box } from '@material-ui/core';
 
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import RoutesName from '../../../routes-list';
 
 import useStyles from '../_header-navbar';
-import Drawer from '@material-ui/core/Drawer';
 import TemporaryDrawer from '../../drawer';
-import List from '@material-ui/core/List';
 import CollapsingItem from '../../Product-detail/Product-detail-collapse/collapsing-item';
-import ListItem from '@material-ui/core/ListItem';
 
 export default function NavBar({ toggleCatalog, hideCatalog, children }) {
   const classes = useStyles();
@@ -22,57 +22,52 @@ export default function NavBar({ toggleCatalog, hideCatalog, children }) {
   return (
     <>
       <Button onClick={() => toggleDrawer(true)}>Open</Button>
-    <Drawer
-      open={drawer}
-      onClose={() => toggleDrawer(false)}
+      <Drawer
+        open={drawer}
+        onClose={() => toggleDrawer(false)}
       >
-      {/*<Box className={classes.headerMenuList}>*/}
-      <List
-        component="nav"
-        aria-labelledby="nested-list-subheader"
-        className={classes.root}
-      >
-        <CollapsingItem  label="CATALOG" >
+        {/* <Box className={classes.headerMenuList}> */}
+        <List
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          className={classes.root}
+        >
+          <CollapsingItem border={1} label="CATALOG" >
             <CollapsingItem label="COOCKING">
-              <CollapsingItem label="POTS">
-              </CollapsingItem>
-              <CollapsingItem label="FRYING">
-              </CollapsingItem>
+              <CollapsingItem label="POTS" />
+              <CollapsingItem label="FRYING" />
             </CollapsingItem>
-          <CollapsingItem label="DINING">
+            <CollapsingItem label="DINING" />
+            <CollapsingItem label="DRINKING" />
+            <CollapsingItem label="PREPARING" />
           </CollapsingItem>
-          <CollapsingItem label="DRINKING">
-          </CollapsingItem>
-          <CollapsingItem label="PREPARING">
-          </CollapsingItem>
-        </CollapsingItem>
 
-      <ListItem
-        className={`js_header-menu-list-item ${classes.headerMenuListItem}`}
-        onMouseLeave={hideCatalog}
-        onClick={toggleCatalog}
-      >
+          <ListItem
+            className={`js_header-menu-list-item ${classes.headerMenuListItem}`}
+            onMouseLeave={hideCatalog}
+            onClick={toggleCatalog}
+          >
         CATALOG
-      </ListItem>
-      <ListItem>
-        <Link to={RoutesName.aboutUs} className={classes.headerMenuListHyperlink}>
+          </ListItem>
+          <ListItem>
+            <Link to={RoutesName.aboutUs} className={classes.headerMenuListHyperlink}>
           ABOUT US
-        </Link>
-      </ListItem>
-      <ListItem>
-        <Link to={RoutesName.delivery} className={classes.headerMenuListHyperlink}>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to={RoutesName.delivery} className={classes.headerMenuListHyperlink}>
           DELIVERY & PAYMENT TERMS
-        </Link>
-      </ListItem>
-      <ListItem>
-        <Link to={RoutesName.contacts} className={classes.headerMenuListHyperlink}>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to={RoutesName.contacts} className={classes.headerMenuListHyperlink}>
           CONTACTS
-        </Link>
-      </ListItem>
-      {children}
-      </List>
-    </Drawer>
-      </>
+            </Link>
+          </ListItem>
+          {children}
+        </List>
+      </Drawer>
+    </>
   )
 }
 
