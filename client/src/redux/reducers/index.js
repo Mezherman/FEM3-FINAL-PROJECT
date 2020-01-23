bdb33f3c95954133b9247bc491bb7bf0cad594ac
Anna Butrimenko <annettt@gmail.com>
2020-01-23 21:14:47 +0200

    Mrrged develop, fixed conflicts

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -4,10 +4,10 @@ import { reducer as formReducer } from 'redux-form';
 import cart from './cart';
 import categoriesReducer from './categories';
 import productsReducer from './products';
-import formReducer from './form';
+// import formReducer from './form';
 import filterReducer from './filter';
 import userReducer from './user';
-import favoritesReducer from  './favorites';
+import favoritesReducer from './favorites';
 
 export default combineReducers({
   cart,
