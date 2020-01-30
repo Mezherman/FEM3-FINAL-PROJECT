fa2e4d9a4563f58eb264aedd4ffc184dfb964489
Peaceful Warrior <peaceful11111111@gmail.com>
2020-01-30 17:13:53 +0200

    commit

--- client/src/components/Main-carousel/mainCarousel.js
+++ client/src/components/Main-carousel/mainCarousel.js
@@ -1,28 +1,30 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import Carousels from '../Carousel/carousel';
 import PagingDots from '../Carousel/pagingDots';
 import useStylesCarousel from '../Carousel/_carousel';
 import useStylesMainCarousel from './_mainCarousel';
+import getMainSlides from '../../services/getMainSlides';
+import slidesToFetch from '../../redux/actions/slides';
 
 axios.defaults.baseURL = 'http://localhost:5000';
 
-export default function MainSlider() {
-  const [slides, setSlides] = useState([]);
+function MainSlider ({ fetchSlides }) {
+  const { mainSlides } = useSelector((state) => state.slides);
 
   const classes = useStylesCarousel();
   const mainCarouselClasses = useStylesMainCarousel();
 
   useEffect(() => {
-    axios
-      .get('/slides')
-      .then((slidesToShow) => setSlides(slidesToShow.data))
-      .catch((err) => {
-        console.log(err)
-      });
-  }, []);
+    if (mainSlides.length < 1) {
+      getMainSlides()
+        .then((response) => fetchSlides(response.data))
+    }
+  }
+  , [fetchSlides, mainSlides.length]);
 
   return (
     <div className={classes.carouselContainer}>
@@ -57,21 +59,36 @@ export default function MainSlider() {
             // eslint-disable-next-line max-len
           }) => (
             // eslint-disable-next-line no-console,max-len
-            <PagingDots currentSlide={currentSlide} goToSlide={goToSlide} slideCount={slideCount} slidesToScroll={slidesToScroll} slidesToShow={slidesToShow} cellAlign={cellAlign} classItems={mainCarouselClasses.liMainPagingItem} ulPagingItem={mainCarouselClasses.ulMainPagingItem} buttonItems={mainCarouselClasses.buttonMainPagingItem} />
+            <PagingDots
+              currentSlide={currentSlide}
+              goToSlide={goToSlide}
+              slideCount={slideCount}
+              slidesToScroll={slidesToScroll}
+              slidesToShow={slidesToShow}
+              cellAlign={cellAlign}
+              classItems={mainCarouselClasses.liMainPagingItem}
+              ulPagingItem={mainCarouselClasses.ulMainPagingItem}
+              buttonItems={mainCarouselClasses.buttonMainPagingItem}
+            />
           )
         }
       >
-        {slides.map((item) => {
+        {mainSlides.map((item) => {
           // console.log(item);
           const { imageUrl, title, description, route, _id } = item;
           return (
-            <Link to={route} key={_id} >
-              <img src={imageUrl} className={mainCarouselClasses.img} alt="Special proposition for customers" />
+            <Link to={route} key={_id}>
+              <img
+                src={imageUrl}
+                className={mainCarouselClasses.img}
+                alt="Special proposition for customers"
+              />
               <div className={mainCarouselClasses.textBlock}>
                 <h3 className={mainCarouselClasses.title}>{title}</h3>
                 <p className={mainCarouselClasses.description}>{description}</p>
                 <Button
                   size="large"
+                  className={mainCarouselClasses.showMore}
                   variant="contained"
                   color="secondary"
                   disableElevation
@@ -86,3 +103,13 @@ export default function MainSlider() {
     </div>
   )
 }
+
+const mapStateToProps = (state) => ({
+  slides: state.slides.mainSlides
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchSlides: (data) => dispatch(slidesToFetch(data))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainSlider)
