f4f808fed220e6437b51bf522028cb81ee46187c
shpakylia <shpakylia91@gmail.com>
2020-02-02 21:24:15 +0200

    route index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const path = require("path");
 
 router.get("*", (req, res) => {
-  console.log(path.join(__dirname, "../client/build/index.html"));
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
