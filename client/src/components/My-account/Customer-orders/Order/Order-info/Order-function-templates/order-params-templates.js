f4bc47a80ae8c66d88f001ff0fd8be461f39975e
Peaceful Warrior <peaceful11111111@gmail.com>
2020-02-13 20:31:32 +0200

    push

--- client/src/components/My-account/Customer-orders/Order/Order-info/Order-function-templates/order-params-templates.js
+++ client/src/components/My-account/Customer-orders/Order/Order-info/Order-function-templates/order-params-templates.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import useStylesOrderItem from '../../../Order-list/_order-list'
 
-const OrderParamsTemplates = ({ name, value, classContainer, classTitle, classDivider, style }) => {
+const OrderParamsTemplates = ({ name, value, classContainer, classTitle, classDivider }) => {
   const classes = useStylesOrderItem();
   return (
     <Grid component="div" item container className={classContainer}>
@@ -12,7 +12,6 @@ const OrderParamsTemplates = ({ name, value, classContainer, classTitle, classDi
       <span>{value}</span>
       <Divider
         className={`${classes.hiddenCollapse} ${classDivider}`}
-        style={style}
         orientation="horizontal"
         variant="middle"
         component="div"
@@ -28,15 +27,10 @@ OrderParamsTemplates.propTypes = {
   value: PropTypes.string.isRequired,
   classContainer: PropTypes.string,
   classTitle: PropTypes.string,
-  style: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.string,
-  ]),
   classDivider: PropTypes.string
 };
 OrderParamsTemplates.defaultProps = {
   classContainer: '',
   classTitle: '',
-  style: {},
   classDivider: '',
 };
