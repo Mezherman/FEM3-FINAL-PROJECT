80ac1b4179a625bc78705869f152b88b866d5957
mezherman <vlad.mezhik@gmail.com>
2020-02-14 17:28:52 +0200

    search fixing, small refactoring

--- client/src/services/filter.js
+++ client/src/services/filter.js
@@ -23,33 +23,6 @@ function getColors() {
 function getBrands() {
   return axios('/filters/brands').then((response) => (response.data))
 }
-const parseToFilterValue = (obj, sort = '', pages, allCategories, catalogLocation) => {
-  let valOfBrands = '';
-  let valOfPrice = '';
-  let valOfColor = '';
-  const valOfPerPage = `perPage=${pages.perPage}`;
-  const valOfStartPage = `startPage=${pages.startPage}`;
-  const valOfSort = sort.sortName ? `&sortName=${sort.sortName}&sortValue=${sort.sortValue}` : '';
-
-  if (obj.brand.length > 0) {
-    valOfBrands = `brand=${obj.brand.join(',')}`
-  }
-
-  if (obj.price.length > 0) {
-    valOfPrice = `minPrice=${obj.price[0]}&maxPrice=${obj.price[1]}`
-  }
-
-  if (obj.color.length > 0) {
-    valOfColor = `color=${obj.color.join(',')}`
-  }
-
-  const subCategories = allCategories.filter((category) => category.parentId === catalogLocation);
-  const subCategoriesString = subCategories ? subCategories.map((subCategory) => subCategory.id).join(',') : '';
-  const categoryForFilter = !subCategoriesString ? catalogLocation : subCategoriesString;
-
-  const valToFilter = `categories=${categoryForFilter}&${valOfBrands}&${valOfColor}&${valOfPrice}&${valOfPerPage}&${valOfStartPage}${valOfSort}`
-  return valToFilter
-};
 
 const parseToFilterValue = (itemNoArr, filterResults, sort = '', pages, allCategories, catalogLocation) => {
   let valOfBrands = '';
