b044bf618e55d2d37bdd0384048fb0a6a33dda1e
Maksym Pliushch <makcldsnetua@gmail.com>
2020-02-16 02:35:00 +0200

    add carousel & dots test

---
+++ client/src/components/Checkout/checkout.spec.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+import CheckoutStatus from './Status/status'
+import DeliveryPaymentInfo from './Checkout-form/delivery-payment-info'
+import { Typography } from '@material-ui/core';
+
+
+import RadioCheckboxField from './Checkout-form/form-components/radio-checkbox-field'
+import CustomerInfo from './Checkout-form/customer-info'
+
+describe('CheckoutStatus component', () => {
+  const props = {
+    location: {
+      state: {
+        orderDone: true,
+        orderNo: 123,
+        message: 'text'
+      }
+    }
+  };
+
+  it('CheckoutStatus orderDone is true', () => {
+    const wrapper = shallow(<CheckoutStatus {...props} />);
+
+    expect(wrapper.find('h2')).toHaveLength(1)
+    expect(wrapper.find('p').text()).toEqual('Thank you for your choice!')
+  });
+
+  it('CheckoutStatus orderDone is false', () => {
+    const nextProps = {
+      ...props,
+      location: {
+        state: {
+          orderDone: false
+        }
+      }
+    }
+    const wrapper = shallow(<CheckoutStatus {...nextProps} />);
+
+    expect(wrapper.find('h2')).toHaveLength(1)
+    expect(wrapper.find('p')).toHaveLength(2)
+  });
+});
+
+describe('DeliveryPaymentInfo component', () => {
+  it('should render correctly component', () => {
+    const wrapper = shallow(<DeliveryPaymentInfo />);
+
+    expect(wrapper.find('RadioCheckboxField')).toHaveLength(4)
+    expect(wrapper.find(Typography)).toHaveLength(2)
+  });
+})
+
+describe('CustomerInfo component', () => {
+  it('should render correctly component', () => {
+    const props = {
+      customer: 'test'
+    }
+    const wrapper = shallow(<CustomerInfo {...props} />);
+
+    expect(wrapper.find('Field')).toHaveLength(11)
+  });
+})
