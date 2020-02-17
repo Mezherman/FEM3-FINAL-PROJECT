6628cc06303d52f716ec31e7a62ced9a09d24373
Maksym Pliushch <makcldsnetua@gmail.com>
2020-02-17 16:36:05 +0200

    tech

--- client/src/components/Product-card/product-card.js
+++ client/src/components/Product-card/product-card.js
@@ -45,7 +45,7 @@ function ProductCard({ product }) {
         <Divider />
         <div className={classes.iconWrapper}>
           <span className={classes.itemNo}>
-Item.No
+            Item.No
             {itemNo}
           </span>
           {loggedIn && <AddToFavoriteBtn favorites={favorites} itemId={itemId} />}
