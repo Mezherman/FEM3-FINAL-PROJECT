6ca76ea850027a16bccc0a335d00203a422ba096
mezherman <vlad.mezhik@gmail.com>
2020-02-19 19:07:55 +0200

    tech

--- client/src/pages/orders/orders.js
+++ client/src/pages/orders/orders.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 import OrderList from '../../components/My-account/Orders/Order-list/order-list';
 import useProfileStyles from '../../components/My-account/Profile/_profile'
-import HelpPanel from '../../components/Summary-cart/Help-panel/help-panel';
-import ContinueBtn from '../../components/Summary-cart/Continue-btn/continue-btn';
 
 const OrdersPage = () => {
   const profileClasses = useProfileStyles();
@@ -11,8 +9,6 @@ const OrdersPage = () => {
     <Container maxWidth="xl" className={profileClasses.container}>
       <h1>Your Orders</h1>
       <OrderList />
-      <HelpPanel />
-      <ContinueBtn />
     </Container>
   )
 };
